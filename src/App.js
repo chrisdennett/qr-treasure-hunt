824384f3e0d4b2708a99a0520c63352ef87b252e
chrisdennett <chrisdennett@gmail.com>
2019-12-19 16:32:48 +0000

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { Router, Link } from "@reach/router";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>QR Treasure Hunt</h1>
-      </header>
-    </div>
+    <Router>
+      <Home path="/" />
+      <Clue path="clue/:clueId" />
+    </Router>
   );
 }
 
 export default App;
+
+const Home = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1>QR Treasure Hunt</h1>
+    <Link to="/clue/1234">CLUE 1</Link>
+  </header>
+);
+
+const Clue = ({ clueId }) => (
+  <div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Clue: {clueId}</h1>
+    </header>
+  </div>
+);
